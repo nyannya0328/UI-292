df830ac9497fd7390761b42fabff0a9c963dd45c
nyannya0328 <thecuba54@gmail.com>
2021-09-01 21:21:10 +0900

    Initial Commit

---
+++ Shared/UI_292App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_292App.swift
+//  Shared
+//
+//  Created by nyannyan0328 on 2021/09/01.
+//
+
+import SwiftUI
+
+@main
+struct UI_292App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
